0da76bda4738587e7082a782659df2f26407a5ef
Eric D. Torres <etorresnotary@gmail.com>
2020-12-14 14:28:09 -0600

    sign up and sign in initial draft

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Card, Form, Button } from "react-bootstrap";
 
 const Signin = () => {
-    return (
-        <div>
-            This is the signin page
-        </div>
-    )
-}
+  return (
+    <Container className="mt-4">
+      <div className="mb-4">
+        If you need to create an account, please <Link to="/">CLICK HERE</Link> to
+        access the Sign-Up form.
+      </div>
+      <Card>
+        <Card.Header>
+          Returning users may use this form to sign into their account:
+        </Card.Header>
+        <Card.Body>
+          <Form className="mt-2">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
 
 export default Signin;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,29 +5,33 @@ import { Container, Card, Form, Button } from "react-bootstrap";
 const Signup = () => {
   return (
     <Container className="mt-4">
-      <div>
-        Returning users <Link to="/signin">CLICK HERE</Link> to access the Sign
-        In form.
+      <div className="mb-4">
+        Returning users <Link to="/signin">CLICK HERE</Link> to access the
+        Sign-In form.
       </div>
-      <Card className="mt-3 p-3">
-        <div>New users may use this form to sign into their account:</div>
-        <Form className="mt-2">
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+      <Card>
+        <Card.Header>
+          New users may use this form to create their account:
+        </Card.Header>
+        <Card.Body>
+          <Form className="mt-2">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
       </Card>
     </Container>
   );
