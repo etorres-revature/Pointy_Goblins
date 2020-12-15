630d8c873c0304666968abc9dc2115acde82c53d
Eric D. Torres <etorresnotary@gmail.com>
2020-12-14 18:58:16 -0600

    removing card, potentially better styling?

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -10,33 +10,32 @@ const Signin = () => {
         to access the Sign-Up form.
       </div>
 
-      <Card>
-        <Card.Header>
-          Returning users may use this form to sign into their account:
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>SIGN-IN FORM</Card.Title>
+      <Card.Header>
+        Returning users may use this form to sign into their account:
+      </Card.Header>
 
-          <Form className="mt-2">
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+      <Card.Body>
+        <Card.Title>SIGN-IN FORM</Card.Title>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
+        <Form className="mt-2">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-            <Button variant="primary" type="submit" block>
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" block>
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
     </Container>
   );
 };
