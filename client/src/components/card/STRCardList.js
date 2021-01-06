63f6ef73393373e307df05eaa82086a0a2b3b9a9
abraspin <abrahamspindel@gmail.com>
2021-01-06 17:42:35 -0600

    add comments to help Abraham remember which cards are which

--- client/src/components/card/STRCardList.js
+++ client/src/components/card/STRCardList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import STRCard from "./STRCard";
-import "./CardList.css"
+import "./CardList.css";
 
+// for landing page search options
 const CardList = (props) => {
-console.log('CARDLIST')
-console.log(props)
+  console.log("CARDLIST");
+  console.log(props);
 
   return (
     <div className="cardList">
       {props.cities.map((city) => {
-        return <STRCard key={city.id} city={city}  />;
+        return <STRCard key={city.id} city={city} />;
       })}
     </div>
   );
