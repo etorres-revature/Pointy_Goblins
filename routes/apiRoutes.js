41547b4f75b3b9d47cfbe5db4d18eed0a097850b
abraspin <abrahamspindel@gmail.com>
2021-01-06 20:42:14 -0600

    attempting web scraping with airbnb only

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,15 @@ const bcrypt = require("bcrypt");
 const passport = require("../config/passport");
 const airbnb = require("../webscrapers/airbnb");
 const vrbo = require("../webscrapers/vrbo");
-const sonder = require("../webscrapers/sonder")
+const sonder = require("../webscrapers/sonder");
 
 module.exports = (app) => {
   app.post("/api/createUser", (request, response) => {
-    request.body.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10), null);
+    request.body.password = bcrypt.hashSync(
+      request.body.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
 
     db.User.create(request.body)
       .then((result) => {
@@ -18,40 +22,42 @@ module.exports = (app) => {
       });
   });
 
-  app.post("/api/signin", passport.authenticate("local"), (request, response) => {
-    response.json(request.user);
-  });
+  app.post(
+    "/api/signin",
+    passport.authenticate("local"),
+    (request, response) => {
+      response.json(request.user);
+    }
+  );
 
-  //GET ALL DATA 
-const getAllListings =  async (location)=>{
-  let allData = []
-  // const city = location.toLowerCase()
-  // const vrboData = await vrbo.getData(city)
-  // // console.log('---------VRBO---------')
-  // // console.log(vrboData)
-  // const sonderData = await sonder.getData(city)
-  // // console.log('---------SONDERS---------')
-  // // console.log(sonderData)
+  //GET ALL DATA
+  const getAllListings = async (location) => {
+    let allData = [];
+    const city = location.toLowerCase();
+    // const vrboData = await vrbo.getData(city)
+    // // console.log('---------VRBO---------')
+    // // console.log(vrboData)
+    // const sonderData = await sonder.getData(city)
+    // // console.log('---------SONDERS---------')
+    // // console.log(sonderData)
 
-  const airbnbData = await airbnb.getData(city)
-  // console.log('---------AIRBNB---------')
-  // console.log(airbnbData)
-  allData.push(...airbnbData)
-  allData.push(...vrboData)
-  allData.push(...sonderData)
-  return allData
-
-}
-
+    const airbnbData = await airbnb.getData(city);
+    // console.log('---------AIRBNB---------')
+    // console.log(airbnbData)
+    allData.push(...airbnbData);
+    // allData.push(...vrboData);
+    // allData.push(...sonderData);
+    return allData;
+  };
 
   app.get("/api/:city", (req, res) => {
     // console.log("$$$$$$$$$$$$$$$", req.params);
-    getAllListings(req.params.city).then((data)=>{
-      console.log('-------COMBINED-DATA-------')
-      console.log(data)
-      return res.json(data)
-    })
-    
+    getAllListings(req.params.city).then((data) => {
+      console.log("-------COMBINED-DATA-------");
+      console.log(data);
+      return res.json(data);
+    });
+
     // vrbo
     //   .getData(req.params.city)
     //   .then((data) => {
