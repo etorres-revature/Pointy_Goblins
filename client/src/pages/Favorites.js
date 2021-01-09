cacb01004233242519e8806d995aaba23f888064
abraspin <abrahamspindel@gmail.com>
2021-01-09 09:25:17 -0600

    move favorites card deck outside map for proper rendering

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -11,43 +11,48 @@ const Search = () => {
 
   function getFavs() {
     API.getFavorites().then((results) => {
-        setListings(results.data);
-      });
-  };
+      setListings(results.data);
+    });
+  }
 
   function deleteListing(event) {
     const id = event.target.value;
 
-    API.deleteFavListing(id).then(result => {
-        console.log(result);
-        getFavs();
+    API.deleteFavListing(id).then((result) => {
+      console.log(result);
+      getFavs();
     });
-  };
+  }
 
   return (
-      listings.map(listing => 
-
-        <CardDeck>
-          <Card>
-            <Card.Img variant="top" src={listing.image} />
-                <Card.Body>
-                <Card.Title>
-                    <strong> {listing.title}</strong>
-                </Card.Title>
-                <hr></hr>
-                <Card.Text>{listing.description}</Card.Text>
-                <h4>
-                ${listing.price} /<small> night</small>
-                </h4>
-                <Button variant="primary" target="_blank" href={listing.link}>
-                See listing on {listing.source}
-                </Button>
-                <Button onClick={deleteListing} value={listing._id} className="btn btn-danger" variant="secondary m-2">Remove from Favorites</Button>
-            </Card.Body>
+    <CardDeck>
+      {listings.map((listing) => (
+        <Card>
+          <Card.Img variant="top" src={listing.image} />
+          <Card.Body>
+            <Card.Title>
+              <strong> {listing.title}</strong>
+            </Card.Title>
+            <hr></hr>
+            <Card.Text>{listing.description}</Card.Text>
+            <h4>
+              ${listing.price} /<small> night</small>
+            </h4>
+            <Button variant="primary" target="_blank" href={listing.link}>
+              See listing on {listing.source}
+            </Button>
+            <Button
+              onClick={deleteListing}
+              value={listing._id}
+              className="btn btn-danger"
+              variant="secondary m-2"
+            >
+              Remove from Favorites
+            </Button>
+          </Card.Body>
         </Card>
-        </CardDeck>
-    
-      )
+      ))}
+    </CardDeck>
   );
 };
 
