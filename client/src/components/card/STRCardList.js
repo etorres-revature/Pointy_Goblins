ba170983c7745e9466adb442d37b0340242a55c5
abraspin <abrahamspindel@gmail.com>
2021-01-09 09:26:45 -0600

    put landing page cardslist in a card group and center

--- client/src/components/card/STRCardList.js
+++ client/src/components/card/STRCardList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import STRCard from "./STRCard";
 import "./CardList.css";
+import { Card, CardGroup } from "react-bootstrap";
 
 // for landing page search options
 const CardList = (props) => {
@@ -8,11 +9,13 @@ const CardList = (props) => {
   console.log(props);
 
   return (
-    <div className="cardList">
-      {props.cities.map((city) => {
-        return <STRCard key={city.id} city={city} />;
-      })}
-    </div>
+    <CardGroup>
+      <div className="cardList mx-auto">
+        {props.cities.map((city) => {
+          return <STRCard key={city.id} city={city} />;
+        })}
+      </div>
+    </CardGroup>
   );
 };
 
