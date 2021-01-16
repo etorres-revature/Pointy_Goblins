ded664be75ab1aa78c76ecb13ea4ee5c5f3e5751
Eric D. Torres <etorresnotary@gmail.com>
2021-01-16 11:39:06 -0600

    fixing apiRoutes?

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -74,74 +74,6 @@ module.exports = (app) => {
   });
 
   app.delete("/api/delete/:id", (req, res) => {
-    db.FavoriteListing.findOneAndDelete({ "_id": req.params.id }).then(result => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err);
-    })
-  })
-
-  app.get("/api/logout", (req, res) => {
-    req.logout();
-    res.json({ message: "logged out" })
-  })
-
-  //Get all data from DB (previously scraped by auto-script)
-  const getAllListings = async (location) => {
-    switch (location) {
-      case "BOSTON":
-        const bostonData = await db.Boston.findOne({}, {}, { sort: { 'createdAt': -1 } }, function (err, post) {
-          try {
-            return post
-          } catch {
-            (error) => {
-              console.log(error)
-            }
-          }
-
-        });
-        return bostonData.results
-
-      case "AUSTIN":
-        const austinData = await db.Austin.findOne({}, {}, { sort: { 'createdAt': -1 } }, function (err, post) {
-          try {
-            return post
-          } catch {
-            (error) => {
-              console.log(error)
-            }
-          }
-
-        });
-        return austinData.results
-
-      case "HOUSTON":
-        const houstonData = await db.Houston.findOne({}, {}, { sort: { 'createdAt': -1 } }, function (err, post) {
-          try {
-            return post
-          } catch {
-            (error) => {
-              console.log(error)
-            }
-          }
-
-        });
-        return houstonData.results
-
-      case "DENVER":
-        const denverData = await db.Denver.findOne({}, {}, { sort: { 'createdAt': -1 } }, function (err, post) {
-          try {
-            return post
-          } catch {
-            (error) => {
-              console.log(error)
-            }
-          }
-
-        });
-        return denverData.results
-
-  app.delete("/api/delete/:id", (req, res) => {
     db.FavoriteListing.findOneAndDelete({ _id: req.params.id })
       .then((result) => {
         res.json(result);
@@ -156,17 +88,7 @@ module.exports = (app) => {
     res.json({ message: "logged out" });
   });
 
-  app.delete("/api/budgetItem/:id", (req, res) => {
-    const removeId = req.params.id;
-
-    db.User.findById(req.user._id).then((result) => {
-      result.budgetItems.id(removeId).remove();
-      result.save();
-      res.json({ message: "Object was removed" });
-    });
-  });
-
-  //GET ALL DATA
+  //Get all data from DB (previously scraped by auto-script)
   const getAllListings = async (location) => {
     switch (location) {
       case "BOSTON":
@@ -237,14 +159,112 @@ module.exports = (app) => {
         );
         return denverData.results;
 
-      default:
-        return;
+        app.delete("/api/delete/:id", (req, res) => {
+          db.FavoriteListing.findOneAndDelete({ _id: req.params.id })
+            .then((result) => {
+              res.json(result);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+
+        app.get("/api/logout", (req, res) => {
+          req.logout();
+          res.json({ message: "logged out" });
+        });
+
+        app.delete("/api/budgetItem/:id", (req, res) => {
+          const removeId = req.params.id;
+
+          db.User.findById(req.user._id).then((result) => {
+            result.budgetItems.id(removeId).remove();
+            result.save();
+            res.json({ message: "Object was removed" });
+          });
+        });
+
+        //GET ALL DATA
+        const getAllListings = async (location) => {
+          switch (location) {
+            case "BOSTON":
+              const bostonData = await db.Boston.findOne(
+                {},
+                {},
+                { sort: { createdAt: -1 } },
+                function (err, post) {
+                  try {
+                    return post;
+                  } catch {
+                    (error) => {
+                      console.log(error);
+                    };
+                  }
+                }
+              );
+              return bostonData.results;
+
+            case "AUSTIN":
+              const austinData = await db.Austin.findOne(
+                {},
+                {},
+                { sort: { createdAt: -1 } },
+                function (err, post) {
+                  try {
+                    return post;
+                  } catch {
+                    (error) => {
+                      console.log(error);
+                    };
+                  }
+                }
+              );
+              return austinData.results;
+
+            case "HOUSTON":
+              const houstonData = await db.Houston.findOne(
+                {},
+                {},
+                { sort: { createdAt: -1 } },
+                function (err, post) {
+                  try {
+                    return post;
+                  } catch {
+                    (error) => {
+                      console.log(error);
+                    };
+                  }
+                }
+              );
+              return houstonData.results;
+
+            case "DENVER":
+              const denverData = await db.Denver.findOne(
+                {},
+                {},
+                { sort: { createdAt: -1 } },
+                function (err, post) {
+                  try {
+                    return post;
+                  } catch {
+                    (error) => {
+                      console.log(error);
+                    };
+                  }
+                }
+              );
+              return denverData.results;
+
+            default:
+              return;
+          }
+        };
+
+        app.get("/api/:city", (req, res) => {
+          getAllListings(req.params.city).then((data) => {
+            return res.json(data);
+          });
+        });
     }
   };
-
-  app.get("/api/:city", (req, res) => {
-    getAllListings(req.params.city).then((data) => {
-      return res.json(data);
-    });
-  });
 };
